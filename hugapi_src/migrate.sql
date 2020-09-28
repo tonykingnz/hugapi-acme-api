008490a0d1711c83db4e0e3e9bc82a527abfe3b6
José Antônio Maria de Rezende Silva <joseant.silva@icloud.com>
2020-09-28 11:29:18 -0300

    Updated migrate.sql with store_item table

--- hugapi_src/migrate.sql
+++ hugapi_src/migrate.sql
@@ -3,3 +3,14 @@ create table if not exists store(
 	name text unique not null,
 	address text not null
 );
+
+create type category_type as enum ('service', 'product');
+
+create table if not exists store_item(
+	store_item_id serial primary key,
+	name text unique not null,
+	unit text not null,
+	image text,
+	category category_type not null,
+	lastPrice money not null
+);
